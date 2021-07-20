64c01c7fc19241dd1fce55273a99f298a5237ed0
Markus Rutström <markusrutstrom@gmail.com>
2021-07-20 21:15:14 +0200

    sidebar with toggle

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
-import Aurelia, { /*, StyleConfiguration*/ } from 'aurelia';
-import { MyApp } from './my-app';
-// Css files imported in this main file are NOT processed by style-loader
-// They are for sharedStyles in shadowDOM.
-// However, css files imported in other js/ts files are processed by style-loader.
-// import shared from './shared.scss';
+import Aurelia, { StyleConfiguration } from "aurelia";
+import { MyApp } from "./my-app";
+import styles from "./styles/variables.scss";
 
-Aurelia
-  /*
-  .register(StyleConfiguration.shadowDOM({
-    // optionally add the shared styles for all components
-    sharedStyles: [shared]
-  }))
-  */
+Aurelia.register(
+  StyleConfiguration.shadowDOM({
+    sharedStyles: [styles],
+  })
+)
   .app(MyApp)
   .start();
--- webpack.config.js
+++ webpack.config.js
@@ -1,116 +1,137 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-const cssLoader = 'css-loader';
+const cssLoader = "css-loader";
 
 const sassLoader = {
-  loader: 'sass-loader',
+  loader: "sass-loader",
   options: {
     sassOptions: {
-      includePaths: ['node_modules']
-    }
-  }
+      includePaths: ["node_modules"],
+    },
+  },
 };
 
 const postcssLoader = {
-  loader: 'postcss-loader',
+  loader: "postcss-loader",
   options: {
     postcssOptions: {
-      plugins: ['autoprefixer']
-    }
-  }
+      plugins: ["autoprefixer"],
+    },
+  },
 };
 
-module.exports = function(env, { analyze }) {
-  const production = env.production || process.env.NODE_ENV === 'production';
+module.exports = function (env, { analyze }) {
+  const production = env.production || process.env.NODE_ENV === "production";
   return {
-    target: 'web',
-    mode: production ? 'production' : 'development',
-    devtool: production ? undefined : 'eval-cheap-source-map',
+    target: "web",
+    mode: production ? "production" : "development",
+    devtool: production ? undefined : "eval-cheap-source-map",
     entry: {
-      entry: './src/main.ts'
+      entry: "./src/main.ts",
     },
     output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: production ? '[name].[contenthash].bundle.js' : '[name].bundle.js'
+      path: path.resolve(__dirname, "dist"),
+      filename: production
+        ? "[name].[contenthash].bundle.js"
+        : "[name].bundle.js",
     },
     resolve: {
-      extensions: ['.ts', '.js'],
-      modules: [path.resolve(__dirname, 'src'), 'node_modules']
+      extensions: [".ts", ".js"],
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
     },
     devServer: {
       historyApiFallback: true,
       open: !process.env.CI,
-      port: 9000
+      port: 9000,
     },
     module: {
       rules: [
-        { test: /\.(png|gif|jpg|cur)$/i, loader: 'url-loader', options: { limit: 8192 } },
-        { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff2' } },
-        { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff' } },
-        { test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'file-loader' },
+        {
+          test: /\.(png|gif|jpg|cur)$/i,
+          loader: "url-loader",
+          options: { limit: 8192 },
+        },
+        {
+          test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+          loader: "url-loader",
+          options: { limit: 10000, mimetype: "application/font-woff2" },
+        },
+        {
+          test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+          loader: "url-loader",
+          options: { limit: 10000, mimetype: "application/font-woff" },
+        },
+        {
+          test: /\.(ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+          loader: "file-loader",
+        },
         {
           test: /\.css$/i,
           // For style loaded in src/main.js, it's not loaded by style-loader.
           // It's for shared styles for shadow-dom only.
           issuer: /[/\\]src[/\\]main\.(js|ts)$/,
-          use: [ cssLoader, postcssLoader ]
+          use: [cssLoader, postcssLoader],
         },
         {
           test: /\.scss$/i,
           // For style loaded in src/main.js, it's not loaded by style-loader.
           // It's for shared styles for shadow-dom only.
           issuer: /[/\\]src[/\\]main\.(js|ts)$/,
-          use: [ cssLoader, postcssLoader, sassLoader ]
+          use: [cssLoader, postcssLoader, sassLoader],
         },
         {
           test: /\.css$/i,
           // For style loaded in other js/ts files, it's loaded by style-loader.
           // They are directly injected to HTML head.
           issuer: /(?<![/\\]src[/\\]main)\.(js|ts)$/,
-          use: [ 'style-loader', cssLoader, postcssLoader ]
+          use: ["style-loader", cssLoader, postcssLoader],
         },
         {
           test: /\.scss$/i,
           // For style loaded in other js/ts files, it's loaded by style-loader.
           // They are directly injected to HTML head.
           issuer: /(?<![/\\]src[/\\]main)\.(js|ts)$/,
-          use: [ 'style-loader', cssLoader, postcssLoader, sassLoader ]
+          use: ["style-loader", cssLoader, postcssLoader, sassLoader],
         },
         {
           test: /\.css$/i,
           // For style loaded in html files, Aurelia will handle it.
           issuer: /\.html$/,
-          use: [ cssLoader, postcssLoader ]
+          use: [cssLoader, postcssLoader],
         },
         {
           test: /\.scss$/i,
           // For style loaded in html files, Aurelia will handle it.
           issuer: /\.html$/,
-          use: [ cssLoader, postcssLoader, sassLoader ]
+          use: [cssLoader, postcssLoader, sassLoader],
         },
-        { test: /\.ts$/i, use: ['ts-loader', '@aurelia/webpack-loader'], exclude: /node_modules/ },
+        {
+          test: /\.ts$/i,
+          use: ["ts-loader", "@aurelia/webpack-loader"],
+          exclude: /node_modules/,
+        },
         {
           test: /[/\\]src[/\\].+\.html$/i,
           use: {
-            loader: '@aurelia/webpack-loader',
+            loader: "@aurelia/webpack-loader",
             options: {
               // The other possible Shadow DOM mode is 'closed'.
               // If you turn on "closed" mode, there will be difficulty to perform e2e
               // tests (such as Cypress). Because shadowRoot is not accessible through
               // standard DOM APIs in "closed" mode.
-              defaultShadowOptions: { mode: 'open' }
-            }
+              defaultShadowOptions: { mode: "open" },
+            },
           },
-          exclude: /node_modules/
-        }
-      ]
+          exclude: /node_modules/,
+        },
+      ],
     },
     plugins: [
-      new HtmlWebpackPlugin({ template: 'index.html' }),
-      analyze && new BundleAnalyzerPlugin()
-    ].filter(p => p)
-  }
-}
+      new HtmlWebpackPlugin({ template: "index.html" }),
+      analyze && new BundleAnalyzerPlugin(),
+    ].filter((p) => p),
+  };
+};
